75935cdb9e2f531fe412bca1813f0a6fa74350d8
MarinF-Lab <isaac.marin.flo@gmail.com>
2026-08-01 19:47:44 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const gems = document.querySelectorAll(".gem");
+
+gems.forEach(gem => {
+
+    gem.addEventListener("click", () => {
+
+        gems.forEach(g => g.classList.remove("active"));
+
+        gem.classList.add("active");
+
+    });
+
+});
